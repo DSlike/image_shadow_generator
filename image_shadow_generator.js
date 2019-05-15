61ba2a2a4efee9000b643dd8dc1c13473ae2eaa8
Daniel <danyjunor96@gmail.com>
2019-05-15 19:32:57 +0300

    ES6 style class component

--- image_shadow_generator.js
+++ image_shadow_generator.js
@@ -1,4 +1,6 @@
-const _getPixelsColor = (img) => {
+class ImageShadowGenerator{
+  _getPixelsColor(img) {
+    img.crossOrigin = "Anonymous";
     const canvas = document.createElement('canvas');
     const imgW = img.clientWidth;
     const imgH = img.clientHeight;
@@ -6,7 +8,6 @@ const _getPixelsColor = (img) => {
     canvas.height = imgH;
     const ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0, img.clientWidth, imgH);
-    // document.body.appendChild(canvas);
 
     let ew = imgW*90/100;
     let eh = imgH*90/100;
@@ -19,32 +20,53 @@ const _getPixelsColor = (img) => {
       [...ctx.getImageData(ew,eh,1,1).data],
       [...ctx.getImageData(sw,eh,1,1).data]
     ];
-
     return colors;
   }
-  const _drawShadow = (img) => {
-    let colors = _getPixelsColor(img);
+  _drawShadow(img) {
+    const debug = false;
+    let colors = this._getPixelsColor(img);
     let cl = colors.map((c)=>{
       c[3]=.5;
       return c;
     })
 
-    const shadowBlur = img.clientWidth/5;
-    const shadowSize = img.clientWidth/10;
+    const shadowPos = img.clientWidth/10;
+    const shadowBlur = debug ? 0 : img.clientWidth/5;
+    const shadowSize = debug ? 0 : img.clientWidth/10;
+
+    const spx = [
+      shadowPos-this.offsetX,
+      shadowPos+this.offsetX,
+      shadowPos+this.offsetX,
+      shadowPos-this.offsetX
+    ];
+    const spy = [
+      shadowPos-this.offsetY,
+      shadowPos-this.offsetY,
+      shadowPos+this.offsetY,
+      shadowPos+this.offsetY
+    ];
 
     let shadow = [
-      `-${shadowSize}px -${shadowSize}px ${shadowBlur}px -${shadowSize}px rgba(${cl[0]})`,
-      `${shadowSize}px -${shadowSize}px ${shadowBlur}px -${shadowSize}px rgba(${cl[1]})`,
-      `${shadowSize}px ${shadowSize}px ${shadowBlur}px -${shadowSize}px rgba(${cl[2]})`,
-      `-${shadowSize}px ${shadowSize}px ${shadowBlur}px -${shadowSize}px rgba(${cl[3]})`
+      `-${spx[0]}px -${spy[0]}px ${shadowBlur}px -${shadowSize}px rgba(${cl[0]})`,
+      `${spx[1]}px -${spy[1]}px ${shadowBlur}px -${shadowSize}px rgba(${cl[1]})`,
+      `${spx[2]}px ${spy[2]}px ${shadowBlur}px -${shadowSize}px rgba(${cl[2]})`,
+      `-${spx[3]}px ${spy[3]}px ${shadowBlur}px -${shadowSize}px rgba(${cl[3]})`
     ];
 
     img.style.boxShadow = shadow.join(',');
   }
-  let images = document.getElementsByClassName('_isg');
 
-  Object.keys(images).forEach((i)=>{
-    let img = images[i];
-    img.crossOrigin = "Anonymous";
-    img.onload = ()=>{ _drawShadow(img) };
-  })
+  constructor(config){
+    this.offsetX = config.offsetX | 0;
+    this.offsetY = config.offsetY | 0;
+
+    let images = document.getElementsByClassName('_isg');
+
+    Object.keys(images).forEach((i)=>{
+      let img = images[i];
+      img.crossOrigin = "Anonymous";
+      img.onload = ()=>{ this._drawShadow(img) };
+    })
+  }
+}
