ccab94053db6a7a0219256672df593569c5dd703
Daniel <daniel.votintsev@gmail.com>
2019-05-15 16:45:41 +0300

    Debug mode

--- image_shadow_generator.js
+++ image_shadow_generator.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
     context.drawImage(img, 0, 0);
-    let ew = img.clientWidth/4*3;
-    let eh = img.clientHeight/4*3;
-    let sw = img.clientWidth/4;
-    let sh = img.clientHeight/4;
+
+    let ew = img.clientWidth-20;
+    let eh = img.clientHeight-20;
+    let sw = 20;//img.clientWidth/4;
+    let sh = 20;//img.clientHeight/4;
 
     let colors = [
       [...context.getImageData(sw,sh,1,1).data],
@@ -15,32 +16,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
       [...context.getImageData(sw,eh,1,1).data]
     ];
 
+    console.log(colors);
     return colors;
   }
   const _drawShadow = (img) => {
     let colors = _getPixelsColor(img);
     let cl = colors.map((c)=>{
-      c[3]=0.3;
+      c[3]=.5;
       return c;
     })
-    
+
     const shadowBlur = img.clientWidth/5;
-    const shadowSize = shadowBlur/10;
-    const end = img.clientWidth-shadowSize;
+    const shadowSize = img.clientWidth/10;
+
     let shadow = [
-      `0 0 ${shadowBlur}px -${shadowSize}px rgba(${colors[0]})`,
-      `${shadowSize}px 0 ${shadowBlur}px -${shadowSize}px rgba(${colors[1]})`,
-      `0 ${shadowSize}px ${shadowBlur}px -${shadowSize}px rgba(${colors[2]})`,
-      `${shadowSize}px ${shadowSize}px ${shadowBlur}px -${shadowSize}px rgba(${colors[3]})`
+      `-${shadowSize}px -${shadowSize}px ${0}px -${0}px rgba(${cl[0]})`,
+      `${shadowSize}px -${shadowSize}px ${0}px -${0}px rgba(${cl[1]})`,
+      `${shadowSize}px ${shadowSize}px ${0}px -${0}px rgba(${cl[2]})`,
+      `-${shadowSize}px ${shadowSize}px ${0}px -${0}px rgba(${cl[3]})`
     ];
 
     img.style.boxShadow = shadow.join(',');
+    // document.getElementById('block').style.boxShadow = shadow.join(',');
   }
   let images = document.getElementsByClassName('_isg');
 
   Object.keys(images).forEach((i)=>{
     let img = images[i];
-    img.style.transition = '.3s';
-    img.addEventListener('load', _drawShadow(img))
+    img.onload = ()=>{ _drawShadow(img) };
   })
 });
